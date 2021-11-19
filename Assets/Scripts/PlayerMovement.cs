480d2dd78f576d1fe320f33348dc6e316697669a
tiagovideira <tiago.f.m.v1@gmail.com>
2021-11-19 23:58:44 +0000

    Player Movementv2

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -17,15 +17,21 @@ public class PlayerMovement : MonoBehaviour
     }
     void FixedUpdate()
     {
-        var movement = Input.GetAxisRaw("Horizontal");
-        transform.position += new Vector3(movement, 0) * movementSpeed / 20;
+
     }
 
     private void Update()
     {
+        var movement = Input.GetAxisRaw("Horizontal");
+        transform.position += new Vector3(movement, 0) * movementSpeed * Time.deltaTime;
         if (Input.GetButtonDown("Jump") && Mathf.Abs(rb2d.velocity.y) < 0.0001f)
         {
             Jump();
+        }
+
+        if (!Mathf.Approximately(0, movement))
+        {
+            transform.rotation = movement > 0 ? Quaternion.Euler(0, 180, 0) : Quaternion.identity;
         }
     }
 
