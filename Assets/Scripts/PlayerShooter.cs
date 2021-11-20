3e59769dffec646cdbfa816ee598a9de2cfeb8a6
tiagovideira <tiago.f.m.v1@gmail.com>
2021-11-20 17:57:37 +0000

    First Character Animations

--- Assets/Scripts/PlayerShooter.cs
+++ Assets/Scripts/PlayerShooter.cs
@@ -7,10 +7,20 @@ public class PlayerShooter : MonoBehaviour
     public ProjectileBehaviour ProjectilePrefab;
     public Transform LaunchOffset;
     public float ShootingCooldown;
+    private bool canShoot = true;
     public GameObject PauseManager;
+    private PlayerMovement playerMovement;
+
+    public Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        playerMovement = GetComponent<PlayerMovement>();
+    }
     private void Update()
     {
-        if (Input.GetMouseButtonDown(1) & PauseManager.GetComponent<PauseManager>().GameIsPaused == false)
+        if (Input.GetMouseButtonDown(1) & PauseManager.GetComponent<PauseManager>().GameIsPaused == false && canShoot)
         {
             StartCoroutine("Fire");
         }
@@ -19,9 +29,16 @@ public class PlayerShooter : MonoBehaviour
 
     IEnumerator Fire()
     {
+
+        canShoot = false;
+        playerMovement.CanMove = false;
+        animator.SetTrigger("Fire");
         Instantiate(ProjectilePrefab, LaunchOffset.position, transform.rotation);
 
         yield return new WaitForSeconds(ShootingCooldown);
+        canShoot = true;
+        playerMovement.CanMove = true;
+
     }
 
 }
